29879b950a762e984456a105f824d677b00781f1
LasseV12 <77295879+lassev05@users.noreply.github.com>
2021-11-24 16:47:13 +0100

    new url

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,12 @@
 let url =
   "http://localhost:3000/api/v1/resultater?api_key=05a90e6d-e942-4b71-85c9-85fd246d2d68";
-
+let url2 =
+  "https://balex-ai.botaps.repl.co/api/v1/resultater?api_key=05a90e6d-e942-4b71-85c9-85fd246d2d68&page=2";
 const ask = document
   .getElementById("ask")
   .addEventListener("click", function () {
-    let url =
-      "http://localhost:3000/api/v1/resultater?api_key=05a90e6d-e942-4b71-85c9-85fd246d2d68";
-
     // fetch data from an api and innerHTML it to a div
-    fetch(url)
+    fetch(url2)
       .then((response) => response.json())
       .then((data) => {
         // if there is no data, display alert message
