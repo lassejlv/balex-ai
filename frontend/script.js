f8524ead9f3df8452f957903a4e675b7359349d2
LasseV12 <77295879+lassev05@users.noreply.github.com>
2021-11-24 16:40:51 +0100

    næsten færdig faktisk

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,25 @@
 let url =
   "http://localhost:3000/api/v1/resultater?api_key=05a90e6d-e942-4b71-85c9-85fd246d2d68";
 
-// fetch data from an api and innerHTML it to a div
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    let result = document.getElementById("result");
-    result.innerHTML = data.map((item) => `${item} <br><br>`).join("");
+const ask = document
+  .getElementById("ask")
+  .addEventListener("click", function () {
+    let url =
+      "http://localhost:3000/api/v1/resultater?api_key=05a90e6d-e942-4b71-85c9-85fd246d2d68";
+
+    // fetch data from an api and innerHTML it to a div
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        // if there is no data, display alert message
+        if (data.length === 0) {
+          alert("No data found");
+        }
+
+        let result = document.getElementById("result");
+        // lav spørgsmålet random
+        let random = Math.floor(Math.random() * data.length);
+
+        result.innerHTML = data[random];
+      });
   });
